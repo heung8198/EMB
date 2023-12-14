23498ef0de945571e9aa0156d825d96a2b5297c5
JEONEMB <149032336+JEONEMB@users.noreply.github.com>
2023-12-14 20:24:15 +0900

    New

--- ultrasonic.c
+++ ultrasonic.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-#define DEVICE_FILE "/dev/hcsr04" // 드라이버에 의해 생성된 장치 파일
+#define DEVICE_PATH "/dev/hcsr04" // 사용할 디바이스 파일 경로
 
 int main() {
     int fd;
-    char distance[10];
+    char buffer[20];
 
-    // 장치 파일 열기
-    fd = open(DEVICE_FILE, O_RDONLY);
-    if (fd < 0) {
-        perror("Failed to open the device");
-        return errno;
+    // 디바이스 파일 열기
+    fd = open(DEVICE_PATH, O_RDONLY);
+    if (fd == -1) {
+        perror("Failed to open the device file");
+        return 1;
     }
 
-    // 거리 읽기
-    if (read(fd, distance, sizeof(distance)) < 0) {
-        perror("Failed to read the distance");
-        return errno;
+    // 사용자로부터 거리 측정 명령 받기
+    printf("Press Enter to measure distance, q to quit...\n");
+    while (1) {
+        char c = getchar();
+        if (c == 'q' || c == 'Q') {
+            break; // 종료
+        }
+        
+        // 디바이스 파일에서 거리 읽기
+        ssize_t bytes_read = read(fd, buffer, sizeof(buffer));
+        if (bytes_read == -1) {
+            perror("Failed to read from the device file");
+            break;
+        }
+
+        // 읽은 데이터 출력
+        printf("Distance: %s", buffer);
     }
 
-    printf("Distance: %s cm\n", distance);
-
-    // 장치 파일 닫기
+    // 디바이스 파일 닫기
     close(fd);
 
     return 0;
