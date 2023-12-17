0e1b68caff43af0299e8193170ec136eec1979c6
jjuser1 <ajsirhzz75@konkuk.ac.kr>
2023-12-17 19:41:29 +0900

    temp

---
+++ DHT11/.temperature_humidity_driver.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jjuser/working/term_project/EMB/DHT11/temperature_humidity_driver.mod := printf '%s\n'   temperature_humidity_driver.o | awk '!x[$$0]++ { print("/home/jjuser/working/term_project/EMB/DHT11/"$$0) }' > /home/jjuser/working/term_project/EMB/DHT11/temperature_humidity_driver.mod
