147246a97c3f770355b0fbde7f695c9375609e98
정원 <145631638+jjuser1@users.noreply.github.com>
2023-12-19 00:17:58 +0900

    Update pose_cls.py

--- pose_cls.py
+++ pose_cls.py
@@ -37,14 +37,27 @@ def preprocess_input(frame, input_size):
     frame_normalized = frame_resized / 255.0
     return np.expand_dims(frame_normalized, axis=0)
 
-def postprocess_output(landmarks, output_stride):
-    # MoveNet 출력을 사후 처리하여 실제 이미지 좌표로 변환
+def extract_movenet_landmarks(interpreter, output_stride):
+    """MoveNet 출력에서 랜드마크 추출"""
+    landmarks = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])[0]
     return landmarks * output_stride
 
 def draw_skeleton(frame, landmarks):
-    # landmarks에 따라 스켈레톤 그리기
-    # 예: cv2.line(frame, start_point, end_point, color, thickness)
-    pass
+    """랜드마크를 사용하여 스켈레톤 그리기"""
+    joints = landmarks.reshape(-1, 3)  # 각 관절의 (y, x, confidence) 좌표
+
+    # 관절 연결을 위한 인덱스 쌍 (MoveNet에 따라 조정)
+    limb_indices = [(0, 1), (1, 2), (2, 3), (3, 4), 
+                    (0, 5), (5, 6), (6, 7), (7, 8), 
+                    (0, 9), (9, 10), (10, 11), (11, 12), 
+                    (0, 13), (13, 14), (14, 15), (15, 16)]
+
+    for limb in limb_indices:
+        start_idx, end_idx = limb
+        if joints[start_idx, 2] > 0.5 and joints[end_idx, 2] > 0.5:  # 확신도 검사
+            start_point = (int(joints[start_idx, 1]), int(joints[start_idx, 0]))
+            end_point = (int(joints[end_idx, 1]), int(joints[end_idx, 0]))
+            cv2.line(frame, start_point, end_point, (0, 255, 0), 2)
 
 # MoveNet 모델 입력 크기 및 출력 스트라이드
 input_size = (192, 192)
