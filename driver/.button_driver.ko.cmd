eca195dd641944b1b3ccd753b74252e536d1f387
jjuser1 <ajsirhzz75@konkuk.ac.kr>
2023-12-19 16:57:42 +0900

    execute

---
+++ driver/.button_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jjuser/working/term_project/EMB/driver/button_driver.ko := arm-linux-gnueabihf-ld -r  -EL -z noexecstack   --build-id=sha1  -T scripts/module.lds -o /home/jjuser/working/term_project/EMB/driver/button_driver.ko /home/jjuser/working/term_project/EMB/driver/button_driver.o /home/jjuser/working/term_project/EMB/driver/button_driver.mod.o;  true
