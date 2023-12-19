9f525014ecdf841d9a2b04175aa8deca691fbc2b
JEONEMB <149032336+JEONEMB@users.noreply.github.com>
2023-12-20 00:13:14 +0900

    Update pose_cls3.py

--- movenet/pose_cls3.py
+++ movenet/pose_cls3.py
@@ -27,20 +27,20 @@ def run(estimation_model, classification_model, label_file, camera_id, width, he
         class_name = prob_list[0].label
         cv2.putText(image, class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
-        # Calculation pose classification
-        # 각 랜드마크의 좌표를 추출
-        left_shoulder_y = pose_detector.data.keypoints_with_scores[5][0] # 왼쪽 어깨 y 좌표
-        right_shoulder_y = pose_detector.data.keypoints_with_scores[6][0] # 오른쪽 어깨 y 좌표
-        left_wrist_y = pose_detector.data.keypoints_with_scores[9][0] # 왼쪽 손목 y 좌표
-        right_wrist_y = pose_detector.data.keypoints_with_scores[10][0] # 오른쪽 손목 y 좌표
+       # 양손이 어깨보다 높은지 확인
+        left_shoulder = person.keypoints[5].coordinate # 왼쪽 어깨 좌표
+        right_shoulder = person.keypoints[6].coordinate # 오른쪽 어깨 좌표
+        left_wrist = person.keypoints[9].coordinate # 왼쪽 손목 좌표
+        right_wrist = person.keypoints[10].coordinate # 오른쪽 손목 좌표
 
-        # 양손이 어깨보다 높은지 확인
-        if left_wrist_y < left_shoulder_y and right_wrist_y < right_shoulder_y:
-            class_name = "Hands Up"
+        if left_wrist.y < left_shoulder.y and right_wrist.y < right_shoulder.y:
+            class_name = "손이 어깨보다 높음"
         else:
             prob_list = classifier.classify_pose(person)
             class_name = prob_list[0].label
+
         cv2.putText(image, class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         
         cv2.imshow(estimation_model, image)
         if cv2.waitKey(1) == 27:
