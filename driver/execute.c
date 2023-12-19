369ba0780797c8f9ac1820023fd9bb8764a76448
heung8198 <107354972+heung8198@users.noreply.github.com>
2023-12-19 21:41:48 +0900

    Update execute.c

--- driver/execute.c
+++ driver/execute.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include<fcntl.h>
-#include<termios.h>
-
-static struct termios init_setting, new_setting;
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/gpio.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/ktime.h>
 
 
 void init_keyboard()
@@ -38,7 +39,8 @@ int main(int argc, char** argv) {
 	char key;
 
 	int dev0 = open("/dev/my_button", O_RDONLY); // read only
-	int dev1 = open("/dev/hcsr04", O_RDONLY);
+	int dev1 = open("/dev/hcrs04", O_RDONLY);
+	int dev2 = open("/dev/my_led", O_WRONLY);
 
 	if (dev0 < 0) {
 		printf("gpio_driver Opening was not possible!\n");
@@ -50,44 +52,64 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	if (dev2 < 0) {
+		printf("gpio_driver Opening was not possible!\n");
+		return -1;
+	}
+
 	printf("device opening was sucessful!\n");
 
 	char buffer[2];
-	char distance_buffer[20];
+	int distance;
+	int led[2];
 	ssize_t bytes_read;
+
+	//button state read
+	bytes_read = read(dev0, buffer, sizeof(buffer));
+	//button up state buffer[0], button down state buffer[1]
 
 	init_keyboard();
 
 	char prev_buffer[2] = { 0 }; // 이전 버튼 상태를 저장할 배열 선언
 	int state1 = 0;//button up toggle state
 	int state2 = 0;//button down toggle state
-	printf("program start!\n");
 
 	while (1) {
-		
+
 		key = get_key();
 
-		if(key=='q'){
-			printf("exit this program.\n");
-            		break;
-		}
-		else{
-			bytes_read = read(dev0, buffer, sizeof(buffer)); // 버튼 상태 읽기
-			if (buffer[0] != prev_buffer[0] && buffer[0] == '1') { // 버튼 1이 눌렸는지 확인
-            		state1 = !state1; // 측정 시작
-       			}
-			prev_buffer[0] = buffer[0]; // 현재 버튼 상태를 이전 상태로 저장
-		
+		if (key == '\n') {
+			printf("program start!\n");
+			if (buffer[0] != prev_buffer[0]) { // 버튼 1이 눌렸는지 확인
+				state1 = !state1;
+			}
+			
+			// 현재 버튼 상태를 이전 상태로 저장
+			prev_buffer[0] = buffer[0];
+			
+
 			if (state1 == 1) {
 				printf("Distance: ");
 				fflush(stdout); // 표준 출력 버퍼 비우기
 
-				memset(distance_buffer, 0, sizeof(distance_buffer)); // 버퍼 초기화
-				read(dev1, distance_buffer, sizeof(distance_buffer)); // 새로운 측정값 읽기
-				printf("%s\n", distance_buffer); // 읽은 데이터 출력
+				read(dev1, &distance, sizeof(distance)); // 새로운 측정값 읽기
+				printf("%d cm\n", distance); // 읽은 데이터 출력
+
+				if (distance < 10) {
+					led[0] = 1;
+					write(dev2, &led, sizeof(led));
+				}
+				else {
+					led[0] = 0;
+					write(dev2, &led, sizeof(led));
+				}
 			}
+			
+		
 		}
 		
+
+	
 	}
 
 	close_keyboard();
