7b33c94d2799cbfa543f50fd49f1f4803946f8a9
jjuser1 <ajsirhzz75@konkuk.ac.kr>
2023-12-20 01:51:50 +0900

    button

---
+++ driver/button.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    int fd;
+    char button_state[2];
+    ssize_t bytes_read;
+
+    // 장치 파일 열기
+    fd = open("/dev/my_button", O_RDONLY);
+    if (fd < 0) {
+        printf("Failed to open the device...");
+        return -1;
+    }
+
+    printf("Reading from the button device...\n");
+    while (1) {
+        // 버튼 상태 읽기
+        bytes_read = read(fd, &button_state, sizeof(button_state));
+        if (bytes_read < 0) {
+            perror("Failed to read the device...");
+            break;
+        } else if (bytes_read == 0) {
+            // 데이터가 없으면 다시 시도
+            continue;
+        }
+
+        // 버튼 상태 출력
+        printf("Button 1 (GPIO 16) state: %c\n", button_state[0]);
+        printf("Button 2 (GPIO 20) state: %c\n", button_state[1]);
+
+        // 간단한 딜레이를 두고 다시 읽기
+        sleep(1);
+    }
+
+    // 장치 파일 닫기
+    close(fd);
+    return 0;
+}
+
