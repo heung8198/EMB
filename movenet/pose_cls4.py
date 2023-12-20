a33baf0122326309f2c2932ab7b98aa62d9477e2
정원 <145631638+jjuser1@users.noreply.github.com>
2023-12-20 14:02:36 +0900

    Update pose_cls4.py

--- movenet/pose_cls4.py
+++ movenet/pose_cls4.py
@@ -26,21 +26,25 @@ def run(estimation_model, camera_id, width, height):
         right_shoulder = person.keypoints[6].coordinate
         left_wrist = person.keypoints[9].coordinate
         right_wrist = person.keypoints[10].coordinate
-
+        current_class = "non"
+        
         if left_wrist.y < left_shoulder.y and right_wrist.y < right_shoulder.y:
             cnt_handup += 1
+            current_class = "hand up"
             if cnt_handup >= 5:
                 cap.release()
                 cv2.destroyAllWindows()
                 return "hand up"
         elif left_wrist.y < left_shoulder.y:
             cnt_left_hand_up += 1
+            current_class = "left_hand_up"
             if cnt_left_hand_up >= 5:
                 cap.release()
                 cv2.destroyAllWindows()
                 return "left_hand_up"
         elif right_wrist.y < right_shoulder.y:
             cnt_right_hand_up += 1
+            current_class = "right_hand_up"
             if cnt_right_hand_up >= 5:
                 cap.release()
                 cv2.destroyAllWindows()
@@ -50,7 +54,7 @@ def run(estimation_model, camera_id, width, height):
             cnt_right_hand_up = 0
             cnt_left_hand_up = 0
 
-        cv2.putText(image, "Detecting...", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(image, current_class, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow(estimation_model, image)
 
         if cv2.waitKey(1) & 0xFF in [ord('q'), ord('Q')]:
