84d709a8d8dd3a26afe550c636ed80824f6fa1a0
jjuser1 <ajsirhzz75@konkuk.ac.kr>
2023-12-20 16:19:02 +0900

    ultrasonic_driver

---
+++ driver/.ultrasonic_driver.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jjuser/working/term_project/EMB/driver/ultrasonic_driver.mod := printf '%s\n'   ultrasonic_driver.o | awk '!x[$$0]++ { print("/home/jjuser/working/term_project/EMB/driver/"$$0) }' > /home/jjuser/working/term_project/EMB/driver/ultrasonic_driver.mod
---
+++ driver/ultrasonic_driver.mod
@@ -0,0 +1,1 @@
+/home/jjuser/working/term_project/EMB/driver/ultrasonic_driver.o
