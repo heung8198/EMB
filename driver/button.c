6984098bb5e20b088cb8d46bf6e7187c104bb19b
jjuser1 <ajsirhzz75@konkuk.ac.kr>
2023-12-20 14:40:55 +0900

    version2

--- driver/button.c
+++ driver/button.c
@@ -20,7 +20,7 @@ int main() {
         // 버튼 상태 읽기
         bytes_read = read(fd, &button_state, sizeof(button_state));
         if (bytes_read < 0) {
-            perror("Failed to read the device...");
+            printf("Failed to read the device...\n");
             break;
         } else if (bytes_read == 0) {
             // 데이터가 없으면 다시 시도
@@ -30,17 +30,16 @@ int main() {
         // 버튼 상태 출력
         if (button_state[0] == '1') {
             printf("Button 1 (GPIO 16) state: %c\n", button_state[0]);
-	}
-	if(button_state[1] == '1'){
+            break;
+        }
+        if (button_state[1] == '1') {
             printf("Button 2 (GPIO 20) state: %c\n", button_state[1]);
             break;
-	}
-			}
-        
-        // 딜레이 없이 지속적으로 확인
+        }
     }
 
     // 장치 파일 닫기
     close(fd);
     return 0;
 }
+
